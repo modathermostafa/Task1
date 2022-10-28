0c1457f01450b5877c329142ba1b39f6c5bce1e4
modathermostafa <modathermostafa91@gmail.com>
2022-10-28 17:13:18 +0200

    first commit

---
+++ MyCalculator.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author modat
+ */
+public class MyCalculator {
+    int add(int a , int b){
+        return a+b;
+                
+    }
+       int sub(int a , int b){
+        return a-b;
+       }
+       int div (int a , int b){
+        return a/b;
+       }
+        int mul (int a , int b){
+        return a*b;
+       }
+  
+}
